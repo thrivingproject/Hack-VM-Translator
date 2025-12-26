91bf79bfbae8e2c7339fd5061c3ef848beb520d2
Christian Isaman <christianisaman@outlook.com>
2025-12-26 09:48:23 +0800

    refactor: streamline argument handling in VMTranslator

--- VMTranslator.py
+++ VMTranslator.py
@@ -10,11 +10,11 @@ cw = CodeWriter(vm_path)
 while parser.has_more_lines():
     parser.advance()
     command = parser.command_type()
+    arg_1 = parser.arg_1()
+    arg_2 = parser.arg_2()
     match command:
         case CommandType.C_PUSH | CommandType.C_POP:
-            segment = parser.arg_1()
-            index = parser.arg_2()
-            cw.write_push_pop(command, segment, index)
+            cw.write_push_pop(command, arg_1, arg_2)
         case CommandType.C_ARITHMETIC:
-            cw.write_arithmetic(parser.arg_1())
+            cw.write_arithmetic(arg_1)
 cw.close()
