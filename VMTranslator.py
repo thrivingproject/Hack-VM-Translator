afd2bcfcad74c30c26d9392859f29550d7468aa5
Christian Isaman <christianisaman@outlook.com>
2025-12-26 11:20:28 +0800

    fix: undo shadow code_writer module

--- VMTranslator.py
+++ VMTranslator.py
@@ -5,14 +5,14 @@ from command_type import CommandType
 
 vm_path = sys.argv[1]
 parser = Parser(vm_path)
-code_writer = CodeWriter(vm_path)
+cw = CodeWriter(vm_path)
 
 while parser.has_more_lines():
     parser.advance()
     command = parser.command_type()
     match command:
         case CommandType.C_PUSH | CommandType.C_POP:
-            code_writer.write_push_pop(command, parser.arg_1(), parser.arg_2())
+            cw.write_push_pop(command, parser.arg_1(), parser.arg_2())
         case CommandType.C_ARITHMETIC:
-            code_writer.write_arithmetic(parser.arg_1())
-code_writer.close()
+            cw.write_arithmetic(parser.arg_1())
+cw.close()
