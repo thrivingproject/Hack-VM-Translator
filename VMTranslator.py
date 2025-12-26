e9377f87a17591bca2cb17e9e376ddef600f90e9
Christian Isaman <christianisaman@outlook.com>
2025-12-26 10:21:33 +0800

    fix: only call arg_1 arg_2 when allowed

--- VMTranslator.py
+++ VMTranslator.py
@@ -5,16 +5,14 @@ from command_type import CommandType
 
 vm_path = sys.argv[1]
 parser = Parser(vm_path)
-cw = CodeWriter(vm_path)
+code_writer = CodeWriter(vm_path)
 
 while parser.has_more_lines():
     parser.advance()
     command = parser.command_type()
-    arg_1 = parser.arg_1()
-    arg_2 = parser.arg_2()
     match command:
         case CommandType.C_PUSH | CommandType.C_POP:
-            cw.write_push_pop(command, arg_1, arg_2)
+            code_writer.write_push_pop(command, parser.arg_1(), parser.arg_2())
         case CommandType.C_ARITHMETIC:
-            cw.write_arithmetic(arg_1)
-cw.close()
+            code_writer.write_arithmetic(parser.arg_1())
+code_writer.close()
